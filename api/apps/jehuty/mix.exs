587bbb5048a62c69a27b3b7a6e3b480785b64a5c
Yoshioka <sss.yoshioka@gmail.com>
2023-04-20 05:45:20 +0000

    🎸 feat: soft_delete 追加

--- api/apps/jehuty/mix.exs
+++ api/apps/jehuty/mix.exs
@@ -41,7 +41,8 @@ defmodule Jehuty.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.2"},
       {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"}
+      {:finch, "~> 0.13"},
+      {:ecto_soft_delete, "~> 2.0"}
     ]
   end
 
