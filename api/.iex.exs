39f63b24a9b285bc9c19610e4f89d0d5610f987e
Yoshioka <sss.yoshioka@gmail.com>
2023-04-20 12:19:03 +0000

    🎸 feat: iex 設定

---
+++ api/.iex.exs
@@ -0,0 +1,13 @@
+IO.puts("====-====-====-====-====")
+IO.puts(".iex.exs")
+
+IO.puts("* Import Modules")
+import Ecto.Query
+import Jehuty
+
+IO.puts("* Alias Modules")
+alias Jehuty.Repo
+alias Schemas.Account.User
+# alias Queries.AccountQuery
+
+IO.puts("====-====-====-====-====")
