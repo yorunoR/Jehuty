2838ae8649fcb669dc2fac8be8d99fe526a285f0
Yoshioka <sss.yoshioka@gmail.com>
2023-04-22 13:04:16 +0000

    🎸 feat: api の port を 3000 に変更

--- api/apps/jehuty/lib/jehuty/admin/admin_user_admin.ex
+++ api/apps/jehuty/lib/jehuty/admin/admin_user_admin.ex
@@ -3,7 +3,7 @@ defmodule Jehuty.Admin.AdminUserAdmin do
     [
       %{
         name: "Log Out",
-        url: "http://localhost:4000/admin_user/log_out",
+        url: "http://localhost:3000/admin_user/log_out",
         method: :delete,
         order: 2,
         location: :top,
